d6fe02c60a53088efc5a70adc7196674d9a08a71
harshankumareshan <harshank333@gmail.com>
2024-01-10 11:32:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
-import Blogs from "./pages/Blogs";
+import Blogs from "./pages/blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import UserBlogs from "./pages/UserBlogs";
